e9e94a775238330a550c14499d0d2a645e778ebf
Luis Miguel López Magaña <luismi.lopez@triana.salesianos.edu>
2024-04-22 13:20:28 +0200

    Se añade mensaje desde el controlador

--- ejemplo-con-ramas/src/main/java/com/salesianostriana/dam/ejemploconramas/controladores/MainController.java
+++ ejemplo-con-ramas/src/main/java/com/salesianostriana/dam/ejemploconramas/controladores/MainController.java
@@ -1,13 +1,15 @@
 package com.salesianostriana.dam.ejemploconramas.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 	
 	@GetMapping("/")
-	public String sayHello() {
+	public String sayHello(Model model) {
+		model.addAttribute("mensaje", "Hola Mundo, chavalada");
 		return "index";
 	}
 
